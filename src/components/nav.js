f7bef2419a7c740b6d7791e6e449a45bd985e73e
aPruner <adampruner99@gmail.com>
2020-01-30 15:37:34 -0800

    create a nav component, start messing around with media queries for headers

---
+++ src/components/nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavContainer = styled.nav`
+`
+
+export default function Nav() {
+  return (
+    <NavContainer>
+    </NavContainer>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,48 @@ import React from "react"
 import styled from 'styled-components'
 import "../styles/global.css"
 
+import Nav from "../components/nav"
+
 const AppContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-color: #070923;
 `
 
 const AppHeader = styled.h1`
   font-family: Manaspace;
   font-size: 6em;
   color: #add8e6;
+  margin: 40px 0 10px 0;
+  @media (max-width: 900px) {
+    font-size: 3em;
+    margin: 20px 0 10px 0;
+  }
+`
+
+const AppSubheader = styled.h3`
+  font-family: Manaspace;
+  font-size: 1.5em;
+  color: #add8e6;
+  margin: 5px 0 5px 0;
+  @media (max-width: 900px) {
+    font-size: 1em;
+  }
 `
 
 export default function App() {
   return (
-    <AppContainer>
-      <AppHeader>
-        Adam Pruner
-      </AppHeader>
-    </AppContainer>
+    <>
+      <Nav />
+      <AppContainer>
+        <AppHeader>
+          Adam Pruner
+        </AppHeader>
+        <AppSubheader>
+          Software engineer, game developer, analytics enthusiast
+        </AppSubheader>
+      </AppContainer>
+    </>
   )
 }
