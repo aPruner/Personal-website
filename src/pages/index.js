874b0caa1bcdd9f3f6e7a3cd83e70cac3e25a3b8
aPruner <adampruner99@gmail.com>
2020-01-30 14:45:57 -0800

    add some styles, start building out site project structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,26 @@
 import React from "react"
+import styled from 'styled-components'
+import "../styles/global.css"
 
-export default () => <div>Hello world!</div>
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #070923;
+`
+
+const AppHeader = styled.h1`
+  font-family: Manaspace;
+  font-size: 6em;
+  color: #add8e6;
+`
+
+export default function App() {
+  return (
+    <AppContainer>
+      <AppHeader>
+        Adam Pruner
+      </AppHeader>
+    </AppContainer>
+  )
+}
