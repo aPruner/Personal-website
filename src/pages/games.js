787baaa730e777f277979b4d156ddd34c171ce98
Adam Pruner <adampruner99@gmail.com>
2020-02-04 12:31:55 -1000

    add games and resume page, no functionality yet

---
+++ src/pages/games.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Games() {
+  return (
+    <div>
+      <h1 style={{color: 'white'}}>
+        This is the Games page
+      </h1>
+    </div>
+  )
+}
---
+++ src/pages/resume.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Resume() {
+  return (
+    <div>
+      <h1 style={{color: 'white'}}>
+        This is the resume page
+      </h1>
+    </div>
+  )
+}
