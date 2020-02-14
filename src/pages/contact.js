c1db50c5b109f9f0a9eaf2c19532920a1d3bc1f6
Adam Pruner <adampruner99@gmail.com>
2020-02-14 15:30:38 -0800

    add contact page, edit README.md

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+
+// TODO: Design and build Contact page, which will use the emailService serverless function
+export default function Contact() {
+  return (
+    <div>
+      <h1 style={{color: 'white'}}>
+        This is the Contact page
+      </h1>
+    </div>
+  )
+}
